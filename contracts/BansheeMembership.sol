625e139f4679b880b17f3411ac1235b503797b96
Lamont V <lamontsprin@aol.com>
2024-05-06 07:37:56 -0400

    Add files via upload

---
+++ contracts/BansheeMembership.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BansheeMembership is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Banshee", "BSH") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "http://bansheenft.xyz/";
+    }
+
+    function mint(address to)
+        public returns (uint256)
+    {
+        require(_tokenIdCounter.current() < 3); 
+        _tokenIdCounter.increment();
+        _safeMint(to, _tokenIdCounter.current());
+
+        return _tokenIdCounter.current();
+    }
+}
