76e5d6d2dad4df8750bef9d4096247a4431ea5ec
Lamont V. <lamontsprin@aol.com>
2024-08-04 23:02:29 -0400

    Update BansheeMembership.sol

--- contracts/BansheeMembership.sol
+++ contracts/BansheeMembership.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+//Membership ticket contract
 contract BansheeMembership is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
