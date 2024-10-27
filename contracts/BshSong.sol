7d59c4d473ad3af1dd11ad451db7b5e65866f9d1
Lamont V. <lamontsprin@aol.com>
2024-10-27 11:36:07 -0400

    Update BshSong.sol

--- contracts/BshSong.sol
+++ contracts/BshSong.sol
@@ -3,7 +3,8 @@ pragma solidity 0.8.0;
  
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
- 
+
+// Simple NFT contract for songs and albums to be sold on EVM chains.
 contract BshSong is NFTokenMetadata, Ownable {
  
   constructor() {
